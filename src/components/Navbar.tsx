fcfa69393e0f3a9fa82b97eaac049a046f3fd27f
arjun <arjuno@exalture.com>
2024-12-29 14:51:41 +0530

    navbar menu hide on outside click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,43 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { scroller } from "react-scroll";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  const menuRef = useRef<HTMLDivElement>(null); // Ref for detecting clicks outside
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const navigate = useNavigate();
 
   // Function to scroll to a specific section
   const scrollToSection = (section: string) => {
+    navigate("/");
     scroller.scrollTo(section, {
       duration: 0,
       smooth: true,
     });
   };
 
+  // Close menu on outside click
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/">Sumaya's World</Link>
       </div>
-      <div className={`navbar-links ${isMenuOpen ? "open" : ""}`}>
+      <div className={`navbar-links ${isMenuOpen ? "open" : ""}`} ref={menuRef}>
         <ul>
           <li>
             <div
