615522e74e1dcf271936552e7964cb91acff4cd2
arjun <arjuno@exalture.com>
2024-12-30 22:37:04 +0530

    amazon button added. ui improvement

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -2,9 +2,40 @@ import { useNavigate } from "react-router-dom";
 import "./BookDetails.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+
+const countryLinks = {
+  JP: "https://www.amazon.co.jp/dp/8198072217",
+  UK: "https://www.amazon.co.uk/dp/8198072217",
+  CA: "https://www.amazon.ca/dp/8198072217",
+  AU: "https://www.amazon.com.au/dp/8198072217",
+  FR: "https://www.amazon.fr/dp/8198072217",
+  AE: "https://www.amazon.ae/F2-FACTOR-Sumaya-Siddique/dp/8198072217/ref=mp_s_a_1_1?crid=24HIL8PX5BP6Z&dib=eyJ2IjoiMSJ9.nm7CaUvbYnMC9D9iIkKVa-YzxWmwhNjHOfMl0Rf7PpnGjHj071QN20LucGBJIEps.BTAVJL0scEZmQefHFDLrGZiD9Z3dLIFILcdfgMFiDFI&dib_tag=se&keywords=f2+factor&qid=1735571521&sprefix=f2+factor%2Caps%2C254&sr=8-1",
+  IN: "https://www.amazon.in/dp/8198072217",
+  US: "https://www.amazon.com/dp/8198072217", // Default USA link
+};
 
 const BookDetails = () => {
+  const [link, setLink] = useState(countryLinks.US); // Default to USA link
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchCountryCode = async () => {
+      try {
+        const response = await fetch("https://ipapi.co/json/");
+        const data = await response.json();
+        console.log("data", data);
+        const countryCode = data.country_code;
+        if (countryLinks[countryCode]) {
+          setLink(countryLinks[countryCode]);
+        }
+      } catch (error) {
+        console.error("Error fetching country code:", error);
+      }
+    };
+
+    fetchCountryCode();
+  }, []);
 
   return (
     <div className="book-details-page">
@@ -42,14 +73,25 @@ const BookDetails = () => {
           resilience, second chances, and the power of love to transcend even
           the greatest challenges.
         </p>
-        <a
-          href="https://www.booklove.co.in/product-page/f2-factor"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="buy-button"
-        >
-          Buy on Booklove
-        </a>
+        <div className="button-container">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="buy-button-amazon"
+          >
+            <i className="fa-brands fa-amazon"></i>
+            Buy on Amazon
+          </a>
+          <a
+            href="https://www.booklove.co.in/product-page/f2-factor"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="buy-button-booklove"
+          >
+            Buy on Booklove
+          </a>
+        </div>
       </div>
     </div>
   );
